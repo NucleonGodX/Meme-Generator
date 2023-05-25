942c43f72e328f963000b685cdce5081ca087c9e
NucleonGodX <you@example.com>
2023-05-25 21:44:03 +0530

    Progress made, data file added for testing

--- src/meme.js
+++ src/meme.js
@@ -1,10 +1,13 @@
 import React from "react";
 import data from "./data"
+
 export default function Meme(){
-    function random(){
-        const randomIndex = Math.floor(Math.random() * data.length)
-        const item=data[randomIndex]
-       console.log(item.url)
+    let [memeImage, setMemeImage]=React.useState("")
+    function getMemeImage(){
+        const memesArray=data
+        const randomNo=Math.floor(Math.random()*memesArray.length)
+        setMemeImage(memesArray[randomNo].url)
+        
     }
     return(<main>
     <div className="inputs">
@@ -12,8 +15,11 @@ export default function Meme(){
     <input type="text"placeholder="Bottom text"></input>
     </div>
     <div className="btn">
-    <button onClick={random}>Get a new meme image</button>
+    <button onClick={getMemeImage} >Get a new meme image</button>
     </div>
+    <meme>
+    <img className="image1" src={memeImage}/>
+    </meme>
     </main>
     )
 }
